ba9162d01e332c2679642a337a33d3ecbc0641d2
Doris Le <dorisle@sandiego.edu>
2020-01-14 23:43:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 // Add your functions and code here
 function destructivelyAppendKitten(name) {
   kittens.push("Ralph")
+  return kittens;
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift("Bob")
   return kittens; 
 }
